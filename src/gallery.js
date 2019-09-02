565d084a953999fbde7f2669a0fa863772105131
oddgarden <oddgarden@outlook.com>
2019-09-02 11:22:59 -0700

    minor change on gallery issue

--- src/gallery.js
+++ src/gallery.js
@@ -7,30 +7,30 @@ export class Gallery extends Component {
             <Container className="App">
                 <h1>PHOTO GALLERY</h1>
 
-                <img className="pics" src="/img/d1.jpg" alt="other"></img>
-                <img className="pics" src="/img/d2.jpg" alt="other"></img>
-                <img className="pics" src="/img/d3.jpg" alt="other"></img>
-                <img className="pics" src="/img/d4.jpg" alt="other"></img>
-                <img className="pics" src="/img/d5.jpg" alt="other"></img>
-                <img className="pics" src="/img/d6.jpg" alt="other"></img>
-                <img className="pics" src="/img/d7.jpg" alt="other"></img>
-                <img className="pics" src="/img/d8.jpg" alt="other"></img>
-                <img className="pics" src="/img/d10.jpg" alt="other"></img>
-                <img className="pics" src="/img/d11.jpg" alt="other"></img>
-                <img className="pics" src="/img/d12.jpg" alt="other"></img>
-                <img className="pics" src="/img/d13.jpg" alt="other"></img>
-                <img className="pics" src="/img/d14.jpg" alt="other"></img>
-                <img className="pics" src="/img/d15.jpg" alt="other"></img>
-                <img className="pics" src="/img/d16.jpg" alt="other"></img>
-                <img className="pics" src="/img/d17.jpg" alt="other"></img>
-                <img className="pics" src="/img/d18.jpg" alt="other"></img>
-                <img className="pics" src="/img/d20.jpg" alt="other"></img>
-                <img className="pics" src="/img/d21.jpg" alt="other"></img>
-                <img className="pics" src="/img/d22.jpg" alt="other"></img>
-                <img className="pics" src="/img/d23.jpg" alt="other"></img>
-                <img className="pics" src="/img/d24.jpg" alt="other"></img>
-                <img className="pics" src="/img/d25.jpg" alt="other"></img>
-                <img className="pics" src="/img/d26.jpg" alt="other"></img>
+                <img className="pics" src="/img/d1.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d2.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d3.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d4.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d5.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d6.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d7.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d8.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d10.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d11.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d12.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d13.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d14.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d15.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d16.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d17.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d18.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d20.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d21.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d22.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d23.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d24.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d25.jpg" alt="daycare environment"></img>
+                <img className="pics" src="/img/d26.jpg" alt="daycare environment"></img>
 
 
 
